62afce21d59a34f141f5556f14699dced2006536
ryu-sugi <ryuu085@gmail.com>
2022-02-22 15:27:14 +0900

    shop_cartin.php

---
+++ htdocs/shop/shop_cartin.php
@@ -0,0 +1,57 @@
+<?PHP
+  session_start();
+  session_regenerate_id(true);
+  if(isset($_SESSION['member_login']) ==false) {
+    print 'ようこそゲスト様';
+    print '<a href="member_login.html">会員ログイン</a><br>';
+    print '<br>';
+  }
+  else {
+    print 'ようこそ';
+    print $_SESSION['member_name'];
+    print '様';
+    print '<a href="member_logout.php">ログアウト</a><br>';
+    print '<br>';
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP SHOP SITE</title>
+</head>
+<body>
+
+  <?php
+
+  try
+  {
+
+    $pro_code=$_GET['procode'];
+
+    $cart[]=$pro_code;
+    $_SESSION['cart']=$cart;
+
+    foreach($cart as $key=>$val) {
+      print $val;
+      print '<br>';
+    }
+
+  }
+  catch (Exception $e)
+  {
+    print $e;
+    exit();
+  }
+
+  ?>
+
+  カートに追加しました。 <br>
+  <br>
+  <a href="shop_list.php">商品一覧に戻る</a>
+
+</body>
+</html>
